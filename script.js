4db0d531c7d0e3558f418d33aa5eacf1c2b2c9a6
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-07-18 15:44:34 +0530

    feat : subscribe section

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 5,
-  spaceBetween: 5,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
-  },
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     clickable: true,
-  //   },
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
-});
+window.onload = () => {
+  var swiper = new Swiper(".product-slider", {
+    slidesPerView: 4,
+    // spaceBetween: 5,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  var swiper = new Swiper(".banner-slider", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+};

36ea2b673fdef3c0c92479b2401419726a6d9329
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-07-18 12:26:34 +0530

    feat : chef matt

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 5,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+});

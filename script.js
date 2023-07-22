f2de8d7edc651b93dae37765ae802e49acde3c79
ZakariyaEnacton <zakriyabombaywala@gmail.com>
2023-07-22 19:04:48 +0530

    feat : changes

--- script.js
+++ script.js
@@ -194,60 +194,135 @@ const sidebarToggle = document.querySelector(".sidebar-center");
 const view = document.querySelectorAll(".view-btn");
 const modal = document.querySelector(".modal-container");
 const cardWrapper = document.querySelector(".product-card-wrapper");
+const modalContainer = document.querySelector(".modal-container");
+const modalContentWrapper = document.querySelector(".modal-content-wrapper");
 // events
 
 window.addEventListener("DOMContentLoaded", function () {
   displayCard(cards);
+  displayModal(modals);
 });
 
 function displayCard(items) {
   let displayCards = items
     .map(function (item) {
-      console.log(item);
+      // console.log(item);
       return ` <div class="product-details-card">
-    <div class="product-card-image">
-        <img src="${item.image}">
-    </div>
-    <div class="product-hover">
-        <div class="fav-image">
-            <a href=""><img src="/images/Favorite.png" alt="favicon" class="fav-icon"></a>
-        </div>
-        <div class="cart-image">
-            <a href=""><img src="/images/cart.png" alt="cart" class="cart-icon"></a>
-        </div>
-        <div class="view-image">
-            <a href="" class="view-btn" data-id="cinnamon-honey"><img src="/images/view.png"
-                    alt="cart" class="view-icon"></a>
-        </div>
-    </div>
-    <div class="product-card-bottom">
-        <div class="product-card-bottom-wrapper">
-            <div class="product-title">
-                <h3>${item.title}</h3>
-            </div>
-            <div class="product-price">
-                <div class="old-price">
-                    <h6>${item.oldPrice}</h6>
-                </div>
-                <div class="new-price">
-                    <h6>${item.newPrice}</h6>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`;
+      <div class="product-card-image">
+          <img src="${item.image}">
+      </div>
+      <div class="product-hover">
+          <div class="fav-image">
+              <a href=""><img src="/images/Favorite.png" alt="favicon" class="fav-icon"></a>
+          </div>
+          <div class="cart-image">
+              <a href=""><img src="/images/cart.png" alt="cart" class="cart-icon"></a>
+          </div>
+          <div class="view-image">
+              <a href="" class="view-btn" data-id="cinnamon-honey"><img src="/images/view.png"
+                      alt="cart" class="view-icon"></a>
+          </div>
+      </div>
+      <div class="product-card-bottom">
+          <div class="product-card-bottom-wrapper">
+              <div class="product-title">
+                  <h3>${item.title}</h3>
+              </div>
+              <div class="product-price">
+                  <div class="old-price">
+                      <h6>${item.oldPrice}</h6>
+                  </div>
+                  <div class="new-price">
+                      <h6>${item.newPrice}</h6>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>`;
     })
     .join("");
 
   cardWrapper.innerHTML = displayCards;
 }
 
-function modalCards(items) {
-  let modalCardItem = items.map(function (item) {
-    console.log(item);
-  });
+function displayModal(items) {
+  let dislayModalItems = items
+    .map(function (item) {
+      // console.log(item);
+      return ` <div
+      class="modal-top-image"
+      style="background-image: url(/images/modal-top-img.png)"
+    >
+      <div class="btn-close">
+        <button class="modal-btn">
+          <img src="/images/Close_round.png" alt="" />
+        </button>
+      </div>
+    </div>
+    <!-- modal details content -->
+    <div class="modal-content">
+      <div class="modal-image-view">
+        <div class="modal-image">
+          <img src="${item.image}" alt="image" />
+        </div>
+        <div class="product-share-wrapper">
+          <p class="product-share-title">Share this product:</p>
+          <div class="share-links">
+            <a href="" class="media-links facebook"
+              ><img src="/images/facebook.png" alt=""
+            /></a>
+            <a href="" class="media-links instagram"
+              ><img src="/images/instagram.png" alt=""
+            /></a>
+            <a href="" class="media-links instagram"
+              ><img src="/images/twitter.png" alt=""
+            /></a>
+            <a href="" class="media-links instagram"
+              ><img src="/images/pinterest icon.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+      <div class="product-details">
+        <div class="modal-product-title">
+          <h3>${item.title}</h3>
+        </div>
+        <div class="modal-price-wrapper">
+          <p class="modal-new-price">${item.newPrice}</p>
+          <p class="modal-old-price">${item.oldPrice}</p>
+        </div>
+        <div class="product-description-wrapper">
+          <p class="product-description">${item.description}</p>
+        </div>
+        <div class="add-cart-wrapper">
+          <div class="add-cart-title-wrapper">
+            <p class="add-cart-title">Quantity:</p>
+          </div>
+          <div class="btn-wrapper">
+            <button class="cart-btn btn-decrement">-</button>
+            <p class="count">1</p>
+            <button class="cart-btn btn-increament">+</button>
+          </div>
+        </div>
+        <div class="btn-action-wrapper">
+          <button class="btn-action btn-add-to-cart">Add to cart</button>
+          <button class="btn-action btn-add-to-wishlist">
+            Add to wishlist
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+    class="modal-bottom-image"
+    style="background-image: url(/images/modal-bottom-img.png)"
+  ></div>
+</div>
+    `;
+    })
+    .join("");
+  modalContainer.innerHTML = dislayModalItems;
 }
-modalCards();
+
 toggleBtn.addEventListener("click", function () {
   sidebarToggle.classList.add("hide-sidebar");
   sidebar.classList.add("show-sidebar");
@@ -258,9 +333,9 @@ closeBtn.addEventListener("click", function () {
   sidebarToggle.classList.remove("hide-sidebar");
 });
 
-view.forEach(function (element) {
-  element.addEventListener("click", function (e) {
-    e.preventDefault();
+view.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
     console.log("clicked");
   });
 });
